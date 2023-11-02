8782e816d1139b3306ef39278e8459ceac866ab0
bhomik27 <bhomikmaheshwari27@gmail.com>
2023-11-03 03:12:31 +0530

    added edit button and delete button features for admin routes

--- models/cart.js
+++ models/cart.js
@@ -26,7 +26,7 @@ module.exports = class Cart {
             if (existingProduct) {
                 updatedProduct = { ...existingProduct };
                 updatedProduct.qty = updatedProduct.qty + 1;
-                cart.products = [...cart.products ];
+                cart.products = [...cart.products];
                 cart.products[existingProductIndex] = updatedProduct;
             }
             else {
@@ -41,4 +41,22 @@ module.exports = class Cart {
             })
         })
     }
-}
+
+        static deleteProduct(id, productPrice) {
+            fs.readFile(p, (err, fileContent) => {
+                if (err) {
+                    return;
+                }
+                const updatedCart = { ...JSON.parse(fileContent) };
+                const product = updatedCart.products.find(prod => prod.id === id);
+                const productQty = product.qty;
+                updatedCart.products = updatedCart.products.filter(
+                    prod => prod.id !== id
+                );
+                updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+                fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+                    console.log(err);
+                })
+            });
+        }
+    }
