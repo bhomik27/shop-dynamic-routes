c3c4bb79b2511bbde5cc5188a6286b43426f95b5
bhomik27 <bhomikmaheshwari27@gmail.com>
2023-11-03 23:47:36 +0530

    connected my sql to this node express app

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Bhomik@27'
+});
+
+module.exports = pool.promise();
